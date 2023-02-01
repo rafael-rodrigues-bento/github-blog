a809fbbf7638906f0c35728e3135995a6cdcf03f
rafael <rafael.rodrigues.bento@outlook.com>
2023-01-31 21:16:22 -0300

    Implementado busca por posts

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,9 +1,17 @@
+import React, { ChangeEvent, useState } from "react";
 import { FormContainer } from "./styles";
 interface SearchFormProps {
   numberOfPublications: number;
+  fetchPosts: (q: string) => Promise<void>;
 }
 
-export function SearchForm({numberOfPublications}: SearchFormProps) {
+export function SearchForm({numberOfPublications, fetchPosts}: SearchFormProps) {
+  const [search, setSearch] = useState("")
+
+  function handleSearchPost(event: ChangeEvent<HTMLInputElement>){
+    setSearch(event.target.value)
+  }
+
   return (
     <FormContainer>
       <div>
@@ -11,8 +19,10 @@ export function SearchForm({numberOfPublications}: SearchFormProps) {
         <span>{numberOfPublications} publicações</span>
       </div>
 
-      <input type="text" 
+      <input 
+        type="text" 
         placeholder="Buscar conteúdo"
+        onChange={handleSearchPost}
       />
     </FormContainer>
   )
